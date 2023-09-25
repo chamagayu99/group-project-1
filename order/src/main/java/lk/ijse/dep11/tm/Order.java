cdadadbdc2997bb9b27a41edd21e8f86c7662761
Sanduni Haththotuwa <sandunihaththotuwa@gmail.com>
2023-09-25 19:57:44 +0530

    Add order class file

--- order/src/main/java/lk/ijse/dep11/tm/Order.java
+++ order/src/main/java/lk/ijse/dep11/tm/Order.java
@@ -5,15 +5,36 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.security.PrivateKey;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Order implements Serializable {
 
-    private String orderId;
+    private String cutomerId;
     private String name;
     private String status;
+    private String contact;
+    int burgerQty;
+    int fishbunQty;
+    int pizzaQty;
+    int sandwitchQty;
 
+    public Order(String customerId, String status,int burgerQty, int sandwitchQty, int pizzaQty, int fishbunQty) {
+        this.burgerQty = burgerQty;
+        this.fishbunQty = fishbunQty;
+        this.pizzaQty = pizzaQty;
+        this.sandwitchQty = sandwitchQty;
+        this.cutomerId=customerId;
+        this.status=status;
 
+    }
+
+    public Order(String cutomerId, String name, String contact, String status) {
+        this.cutomerId = cutomerId;
+        this.name = name;
+        this.status = status;
+        this.contact = contact;
+    }
 }
